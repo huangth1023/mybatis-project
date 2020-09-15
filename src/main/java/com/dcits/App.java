c253abab93c8fb0242f635d48bb24d24862681f9
haungth <1090570826@qq.com>
2020-09-16 00:24:08 +0800

    首次测试

--- src/main/java/com/dcits/App.java
+++ src/main/java/com/dcits/App.java
@@ -1,9 +1,13 @@
 package com.dcits;
 
+import com.dcits.mapper.UserMapper;
+import com.dcits.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 /**
  * Hello world!
@@ -15,36 +19,20 @@ public class App {
 
         String resource = "mybatis-config.xml";
 
-//        try {
-//
-//            SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
-//            SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(Resources.getResourceAsStream(resource));
-//
-//            SqlSession sqlSession = sqlSessionFactory.openSession();
-//
-//            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-//            User user = userMapper.findUserById("001");
-//            System.out.println(user);
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
         try {
 
-            ServerSocket serverSocket = new ServerSocket(2222);
+            SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+            SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(Resources.getResourceAsStream(resource));
 
+            SqlSession sqlSession = sqlSessionFactory.openSession();
 
-            Socket socket = new Socket("localhost", 2222);
-            InetAddress inetAddress = socket.getInetAddress();
-            System.out.println(inetAddress.getHostName());
-            System.out.println(inetAddress.getHostAddress());
-            System.out.println(inetAddress.getAddress());
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            User user = userMapper.findUserById("001");
+            System.out.println(user);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
 
     }
 }
